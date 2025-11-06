0825a6a7d781796f813b3eaca4419763a5f2bd7d
Hiltes <szymanekdariusz448@gmail.com>
2025-11-06 16:12:31 +0100

    Splash screen update
    Added new splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,11 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/questmapgps/MainActivity.kt
+++ app/src/main/java/com/example/questmapgps/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.questmapgps.ui.navigation.NavGraph
 import com.example.questmapgps.ui.theme.QuestMapGPSTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             QuestMapGPSTheme {
                 val navController = rememberNavController()
